0c4cb79d3c1ca244f73d1cd8f4a8b3e630355a81
xwp <xwp_fullstack@163.com>
2018-05-13 20:18:22 +0800

    抽取配置类到单独文件

--- manage.py
+++ manage.py
@@ -1,43 +1,15 @@
 # coding=utf-8
 import redis
 from flask import Flask, session
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate, MigrateCommand, Config
 from flask_script import Manager
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
-
-
+from config import Config
 
 # 创建Flask应用程序实例
 app = Flask(__name__)
-
-# 创建配置类
-class Config(object):
-    DEBUG = True
-
-    # 设置SECRET_KEY
-    SECRET_KEY='AsirL8oeqAv3Gzve+VJIapPXS4i5tfr6NFJ34k525UzKV+K3L0nkGFqZyFjcPuAt'
-    # 数据库配置信息
-    # 设置连接路径，库名
-    SQLALCHEMY_DATABASE_URI="mysql://root:mysql@127.0.0.1:3306/ihome"
-    # 关闭追踪数据变更
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # redis配置
-    REDIS_HOST="127.0.0.1"
-    REDIS_PORT=6379
-
-    # session存储配置
-    # session存到redis中
-    SESSION_TYPE = 'redis'
-    # session存储到哪个数据库中
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
-    # 开启session签名
-    SESSION_USE_SIGNER = True
-    # 设置session过期时间
-    PERMANENT_SESSION_LIFETIME = 86400
-
 
 # 通过配置类加载配置
 app.config.from_object(Config)
